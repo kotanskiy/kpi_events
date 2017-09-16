2576708cd25f1aa54c0283104be73dfa6a76ea17
Alexsey Yefimenko <campelstory@gmail.com>
2017-09-17 01:17:17 +0300

    Update settings.py

--- kpi_events/settings.py
+++ kpi_events/settings.py
@@ -100,7 +100,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'kpi_events',
         'USER': 'root',
-        'PASSWORD': 'learnenglish95',
+        'PASSWORD': '',
         'HOST': 'localhost',
     }
 }
