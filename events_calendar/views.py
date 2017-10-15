2bbc6cc0ec5173199acda28d8a84365eaebb224a
kotanskiy <kotanskiy@gitlab.com>
2017-10-15 23:19:51 +0300

    fix

--- events_calendar/views.py
+++ events_calendar/views.py
@@ -383,7 +383,7 @@ def auth_calendar_api(request):
   global event_id
   event_id = str(request.GET.get('event_id'))
   if credential is None or credential.invalid == True:
-    FLOW.params['state'] = xsrfutil.generate_token(settings.SECRET_KEY,
+    FLOW.params['state'] = xsrfutil.generate_token(settings.SECRET_KEY.encode('utf-8', str='strict'),
                        request.user)
     authorize_url = FLOW.step1_get_authorize_url()
     return HttpResponseRedirect(authorize_url)
